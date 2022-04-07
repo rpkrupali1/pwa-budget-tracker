00cb9358d936cd58e3a144ddf297df33589b2b87
krupali pilgulwar <rpkrupali@gmail.com>
2022-04-07 18:37:59 -0500

    indexedDb post data to eb when it is back online

--- public/js/idb.js
+++ public/js/idb.js
@@ -17,8 +17,7 @@ request.onsuccess = function (event) {
 
   // check if app is online, if yes run sendTransaction() function to send all local db data to api
   if (navigator.onLine) {
-    // we haven't created this yet, but we will soon, so let's comment it out for now
-    // uploadPizza();
+    uploadTransaction();
   }
 };
 
@@ -39,3 +38,48 @@ function saveRecord(record) {
   budgetObjectStore.add(record);
 }
 
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_transaction");
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore("new_transaction");
+          // clear all items in your store
+          budgetObjectStore.clear();
+
+          alert("All saved transactions has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", uploadPizza);
